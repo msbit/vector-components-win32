66bc3fa49c226f433334635d7fd1f7a39fd66877
Tom Sullivan <tom@msbit.com.au>
2020-04-15 17:32:00 +1000

    Add `drawLine` method

--- VectorComponents/context.cpp
+++ VectorComponents/context.cpp
@@ -2,6 +2,7 @@
 
 #include "framework.h"
 
+#include "context.h"
 #include "util.h"
 
 namespace context {
@@ -10,24 +11,25 @@ namespace context {
 			const auto canvasX = (int)util::scale((float)range->left, (float)range->right, (float)rect->left, (float)rect->right, (float)x);
 			SetDCPenColor(context, x == 0 ? RGB(0x80, 0x80, 0x80) : RGB(0xD3, 0xD3, 0xD3));
 
-			BeginPath(context);
-			MoveToEx(context, canvasX, rect->top, nullptr);
-			LineTo(context, canvasX, rect->bottom);
-			EndPath(context);
-			StrokePath(context);
+			drawLine(context, { canvasX, rect->top }, { canvasX, rect->bottom });
 		}
 
 		for (auto y = range->top; y <= range->bottom; y++) {
 			const auto canvasY = (int)util::scale((float)range->top, (float)range->bottom, (float)rect->top, (float)rect->bottom, (float)y);
 			SetDCPenColor(context, y == 0 ? RGB(0x80, 0x80, 0x80) : RGB(0xD3, 0xD3, 0xD3));
 
-			BeginPath(context);
-			MoveToEx(context, rect->left, canvasY, nullptr);
-			LineTo(context, rect->right, canvasY);
-			EndPath(context);
-			StrokePath(context);
+			drawLine(context, { rect->left, canvasY }, { rect->right, canvasY });
 		}
 	}
+
+	void drawLine(HDC context, POINT start, POINT end) {
+		BeginPath(context);
+		MoveToEx(context, start.x, start.y, nullptr);
+		LineTo(context, end.x, end.y);
+		EndPath(context);
+		StrokePath(context);
+	}
+
 	void drawVector(HDC context, RECT *rect, std::tuple<float, float> vector, RECT *range) {
 		const auto x = std::get<0>(vector);
 		const auto y = std::get<1>(vector);
@@ -35,10 +37,6 @@ namespace context {
 		const auto canvasX = (int)util::scale((float)range->left, (float)range->right, (float)rect->left, (float)rect->right, x);
 		const auto canvasY = (int)util::scale((float)range->top, (float)range->bottom, (float)rect->top, (float)rect->bottom, y);
 
-		BeginPath(context);
-		MoveToEx(context, (rect->left + rect->right) / 2, (rect->top + rect->bottom) / 2, nullptr);
-		LineTo(context, canvasX, canvasY);
-		EndPath(context);
-		StrokePath(context);
+		drawLine(context, { (rect->left + rect->right) / 2,  (rect->top + rect->bottom) / 2 }, { canvasX, canvasY });
 	}
 }
--- VectorComponents/context.h
+++ VectorComponents/context.h
@@ -4,5 +4,6 @@
 
 namespace context {
 	void drawGrid(HDC, RECT *, RECT *);
+	void drawLine(HDC, POINT, POINT);
 	void drawVector(HDC, RECT *, std::tuple<float, float>, RECT *);
 }
