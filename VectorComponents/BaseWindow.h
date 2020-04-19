abb4aa794c177842911a6e5b04e33e4f04febb41
Tom Sullivan <tom@msbit.com.au>
2020-04-19 15:35:31 +1000

    move definition and declaration of BaseWindow together

--- VectorComponents/BaseWindow.h
+++ VectorComponents/BaseWindow.h
@@ -5,6 +5,25 @@
 template <class Self, class Parent>
 class BaseWindow {
 protected:
-	static LRESULT CALLBACK Loop(HWND, UINT, WPARAM, LPARAM);
+	static LRESULT CALLBACK Loop(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		Self* _this;
+
+		if (message == WM_NCCREATE) {
+			_this = (Self*)((CREATESTRUCT*)lParam)->lpCreateParams;
+			SetWindowLongPtr(window, GWLP_USERDATA, (LONG_PTR)_this);
+		}
+		else {
+			_this = (Self*)GetWindowLongPtr(window, GWLP_USERDATA);
+		}
+
+		if (_this) {
+			return _this->ProcessMessage(window, message, wParam, lParam);
+		}
+		else {
+			return DefWindowProc(window, message, wParam, lParam);
+		}
+	}
+
 	virtual LRESULT ProcessMessage(HWND, UINT, WPARAM, LPARAM) = 0;
 };
